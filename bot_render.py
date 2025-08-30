5cfb104894caa085b71377ea116b7f25b9fdf884
klimbatik <a.aa2000@yandex.ru>
2025-08-30 13:19:21 +0400

    Add files via upload

--- bot_render.py
+++ bot_render.py
@@ -392,42 +392,43 @@ DETAILED_DESCRIPTIONS = {
 
 # ——— ссылки на PDF-файлы ———
 PDF_LINKS = {
-    (18,6,6): "https://drive.google.com/file/d/10R1PoK8lQbcP5fEVVXecMoLymi45tsGW/view?usp=drive_link",
-    (9,9,18): "https://drive.google.com/file/d/1QaMYUJv--n8iLwseG8_MAgz79dggEgg6/view?usp=drive_link",
-    (9,18,9): "https://drive.google.com/file/d/1uRuiDM-csTgk6SGweSkhbGT20yfK1kXd/view?usp=drive_link",
-    (18,9,9): "https://drive.google.com/file/d/10kDSS349TSu9eYaiCo61uWVjx11WOWRA/view?usp=drive_link",
-    (6,5,17): "https://drive.google.com/file/d/1IOKcMbpaRniLBmPL8s-anCwi1eBrr_O9/view?usp=drive_link",
-    (15,20,5): "https://drive.google.com/file/d/1t3mCNby-NCCBE4Pz_EFbuvXsJim9mpqG/view?usp=drive_link",
-    (15,5,8): "https://drive.google.com/file/d/161NMgmh9KDcrK0og17JrHBSloSNYvNmz/view?usp=drive_link",
-    (3,9,12): "https://drive.google.com/file/d/1w69XCIBm3u6XVTXJF893iL3nV_CzgaRJ/view?usp=drive_link",
-    (3,12,9): "https://drive.google.com/file/d/1w69XCIBm3u6XVTXJF893iL3nV_CzgaRJ/view?usp=drive_link",
-    (9,12,3): "https://drive.google.com/file/d/1w69XCIBm3u6XVTXJF893iL3nV_CzgaRJ/view?usp=drive_link",
-    (15,8,11): "https://drive.google.com/file/d/14eTveJvncg3FRsOlGqBuiDD1Vd885BcE/view?usp=drive_link",
-    (9,15,6): "https://drive.google.com/file/d/18wj_PCzN7ZEaUvfmGiDW2AttFdY15snQ/view?usp=drive_link",
-    (6,17,11): "https://drive.google.com/file/d/1krx7t8o2S8cFdp58HES9lyblQq7nfgKt/view?usp=drive_link",
-    (12,19,7): "https://drive.google.com/file/d/1RYUBW4pCeSmsXwcjjTLiWdXHWP1uud2z/view?usp=drive_link",
-    (21,4,10): "https://drive.google.com/file/d/1O27XG5pSIcGbfsNSQILNTbNVdXxYbf5Z/view?usp=drive_link",
-    (12,16,4): "https://drive.google.com/file/d/12EhO882TN6FFZNkV1LV18Gzy6SGTbJaF/view?usp=drive_link",
-    (3,22,19): "https://drive.google.com/file/d/1BBgsTpA_twkhsgAly9i3DtR6fseIMlRa/view?usp=drive_link",
-    (21,10,16): "https://drive.google.com/file/d/1unFYU8JlQPhYPmFgLlaRpDwX49TBP2WE/view?usp=drive_link",
-    (6,8,20): "https://drive.google.com/file/d/1SdzrR0vieHPZsPI4oxAynQ8KUgN2wYkK/view?usp=drive_link",
-    (3,7,22): "https://drive.google.com/file/d/1dM0z8LpAgNZEO2bViZXiJBQssG1MmFZh/view?usp=drive_link",
-    (9,3,21): "https://drive.google.com/file/d/15pb7irKooMODIvkGacYGNQbGgngdp_w-/view?usp=drive_link",
-    (21,7,13): "https://drive.google.com/file/d/1lPwcqfBzC9gUNdC_10QYPavb3v3N-YIS/view?usp=drive_link",
-    (18,6,15): "https://drive.google.com/file/d/1PWq5Vf6nBrL0eZPWXJa4SmLHsdbJIoKc/view?usp=drive_link",
-    (6,20,14): "https://drive.google.com/file/d/1kugwosiU6g31pPujfCZfSo9WGDouzIJ6/view?usp=drive_link",
-    (21,10,7): "https://drive.google.com/file/d/1vl2gBjs_jQBDHakFJBsHr4uU7OaGsPnn/view?usp=drive_link",
-    (3,13,10): "https://drive.google.com/file/d/10_7IQ-bHmJnmmzYLwpF06NDKlRhavJUV/view?usp=drive_link",
-    (12,18,3): "https://drive.google.com/file/d/1e1xcWuo1uYHDLYGJGkzhP1niun92kUUP/view?usp=drive_link",
-    (18,3,12): "https://drive.google.com/file/d/1e1xcWuo1uYHDLYGJGkzhP1niun92kUUP/view?usp=drive_link",
-    (6,14,8): "https://drive.google.com/file/d/1WC9HbCl6PfDasDX1uYM6qcF7nvFO8JcS/view?usp=drive_link",
+    (18,6,6): "https://drive.google.com/file/d/10R1PoK8lQbcP5fEVVXecMoLymi4…/view?usp=sharing",
+    (9,9,18): "https://drive.google.com/file/d/1QaMYUJv--n8iLwseG8_MAgz79dg…/view?usp=sharing",
+    (9,18,9): "https://drive.google.com/file/d/1uRuiDM-csTgk6SGweSkhbGT20yf…/view?usp=sharing",
+    (18,9,9): "https://drive.google.com/file/d/10kDSS349TSu9eYaiCo61uWVjx11…/view?usp=sharing",
+    (6,5,17): "https://drive.google.com/file/d/1IOKcMbpaRniLBmPL8s-anCwi1eB…/view?usp=sharing",
+    (15,20,5): "https://drive.google.com/file/d/1t3mCNby-NCCBE4Pz_EFbuvXsJim…/view?usp=sharing",
+    (15,5,8): "https://drive.google.com/file/d/161NMgmh9KDcrK0og17JrHBSloSN…/view?usp=sharing",
+    (3,9,12): "https://drive.google.com/file/d/1w69XCIBm3u6XVTXJF893iL3nV_C…/view?usp=sharing",
+    (3,12,9): "https://drive.google.com/file/d/1w69XCIBm3u6XVTXJF893iL3nV_C…/view?usp=sharing",
+    (9,12,3): "https://drive.google.com/file/d/1w69XCIBm3u6XVTXJF893iL3nV_C…/view?usp=sharing",
+    (15,8,11): "https://drive.google.com/file/d/14eTveJvncg3FRsOlGqBuiDD1Vd8…/view?usp=sharing",
+    (9,15,6): "https://drive.google.com/file/d/18wj_PCzN7ZEaUvfmGiDW2AttFdY…/view?usp=sharing",
+    (6,17,11): "https://drive.google.com/file/d/1krx7t8o2S8cFdp58HES9lyblQq7…/view?usp=sharing",
+    (12,19,7): "https://drive.google.com/file/d/1RYUBW4pCeSmsXwcjjTLiWdXHWP1…/view?usp=sharing",
+    (21,4,10): "https://drive.google.com/file/d/1O27XG5pSIcGbfsNSQILNTbNVdXx…/view?usp=sharing",
+    (12,16,4): "https://drive.google.com/file/d/12EhO882TN6FFZNkV1LV18Gzy6SG…/view?usp=sharing",
+    (3,22,19): "https://drive.google.com/file/d/1BBgsTpA_twkhsgAly9i3DtR6fse…/view?usp=sharing",
+    (21,10,16): "https://drive.google.com/file/d/1unFYU8JlQPhYPmFgLlaRpDwX49T…/view?usp=sharing",
+    (6,8,20): "https://drive.google.com/file/d/1SdzrR0vieHPZsPI4oxAynQ8KUgN…/view?usp=sharing",
+    (3,7,22): "https://drive.google.com/file/d/1dM0z8LpAgNZEO2bViZXiJBQssG1…/view?usp=sharing",
+    (9,3,21): "https://drive.google.com/file/d/15pb7irKooMODIvkGacYGNQbGgng…/view?usp=sharing",
+    (21,7,13): "https://drive.google.com/file/d/1lPwcqfBzC9gUNdC_10QYPavb3v3…/view?usp=sharing",
+    (18,6,15): "https://drive.google.com/file/d/1PWq5Vf6nBrL0eZPWXJa4SmLHsdb…/view?usp=sharing",
+    (6,20,14): "https://drive.google.com/file/d/1kugwosiU6g31pPujfCZfSo9WGDo…/view?usp=sharing",
+    (21,10,7): "https://drive.google.com/file/d/1vl2gBjs_jQBDHakFJBsHr4uU7Oa…/view?usp=sharing",
+    (3,13,10): "https://drive.google.com/file/d/10_7IQ-bHmJnmmzYLwpF06NDKlRh…/view?usp=sharing",
+    (12,18,3): "https://drive.google.com/file/d/1e1xcWuo1uYHDLYGJGkzhP1niun9…/view?usp=sharing",
+    (18,3,12): "https://drive.google.com/file/d/1e1xcWuo1uYHDLYGJGkzhP1niun9…/view?usp=sharing",
+    (6,14,8): "https://drive.google.com/file/d/1WC9HbCl6PfDasDX1uYM6qcF7nvF…/view?usp=sharing",
 }
 
 # ——— кнопки ———
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="🔮 Рассчитать кармический хвост")],
-        [KeyboardButton(text="ℹ️ О проекте")]
+        [KeyboardButton(text="ℹ️ О проекте")],
+        [KeyboardButton(text="🔮 Сделать полный анализ")]
     ],
     resize_keyboard=True
 )
@@ -495,10 +496,10 @@ async def handle_date(message: Message):
         ]
     )
 
-    # Кнопка "Скачать PDF"
+    # Кнопка "Скачать PDF" — только кнопка, без текста
     download_button = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="📥 Скачать PDF", url=PDF_LINKS.get(tail_triplet, "#"))]
+            [InlineKeyboardButton(text="📥", url=PDF_LINKS.get(tail_triplet, "#"))]
         ]
     )
 
@@ -509,7 +510,7 @@ async def handle_date(message: Message):
         f"{detailed_text}",
         reply_markup=read_button
     )
-    await message.answer("📥 <b>Скачать PDF</b>", reply_markup=download_button)
+    await message.answer("📥", reply_markup=download_button)
 
 @dp.message(F.text == "ℹ️ О проекте")
 async def about(message: Message):
@@ -518,6 +519,31 @@ async def about(message: Message):
         "Разработан с любовью для тех, кто ищет глубину, смысл и магию в жизни.\n\n"
         f"Канал: <a href='{CHANNEL_LINK}'>@Master_Mystic</a>"
     )
+
+@dp.message(F.text == "🔮 Сделать полный анализ")
+async def full_analysis(message: Message):
+    # Сообщение с ценой и кнопками
+    analysis_button = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="✅ Продолжить", url="https://t.me/Mattrehka")],
+            [InlineKeyboardButton(text="⏸️ Я подумаю", callback_data="think")]
+        ]
+    )
+    await message.answer(
+        "📊 <b>Полный анализ по вашей Матрице Судьбы будет стоить 1000 рублей.</b>\n\n"
+        "🔹 Информация о вас будет передана мне в личные сообщения.\n"
+        "🔹 Мы проведём глубокий разбор вашей судьбы, кармы и путей развития.",
+        reply_markup=analysis_button
+    )
+
+@dp.callback_query(F.data == "think")
+async def think_callback(callback):
+    # Отправляем уведомление тебе
+    await bot.send_message(
+        chat_id=YOUR_TELEGRAM_ID,  # ❗️ Замени на свой ID
+        text=f"🔔 Клиент {callback.from_user.username} ({callback.from_user.id}) нажал 'Я подумаю'"
+    )
+    await callback.answer("💡 Хорошо, подумайте. Я всегда рядом.")  # Ответ пользователю
 
 # ——— запуск вебхука ———
 async def main():
@@ -535,6 +561,4 @@ async def main():
         await runner.cleanup()
 
 if __name__ == "__main__":
-
     asyncio.run(main())
-
